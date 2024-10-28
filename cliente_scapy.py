712420910a5280fd3c8cda54e235214dfe034997
BiaMouraa <beatriz.moura3@academico.ufpb.br>
2024-10-27 22:36:43 -0300

    finalizado

--- cliente_scapy.py
+++ cliente_scapy.py
@@ -1,7 +1,5 @@
 from scapy.all import *
 import random
-import struct
-from ipaddress import IPv4Address
 
 # Configura o IP e a porta do servidor
 IP_SERV = '15.228.191.109'
@@ -32,6 +30,26 @@ def montar_req(tipo):
     
     return bytes(mensagem), identificador
 
+
+def pseudo_cabec_ip(ip_origem, ip_destino, tam):
+    # Faz a conversão para bbytes, para que seja possivel concatenar
+    ip_origem = socket.inet_aton(ip_origem)
+    ip_destino = socket.inet_aton(ip_destino)
+    
+    # Pseudocabecalho 
+    pseudocabecalho = (ip_origem + ip_destino # Os endereços possuem 4 bytes cada
+                       + struct.pack('!BBH', # Especifica a formatação e o tipo para cada campo da struct
+                                        0, # Byte reservado - 1 byte
+                                        17, # Protocolo UDP - 1 byte
+                                        tam)) # Comprimento UDP - 2 bytes
+    return pseudocabecalho
+
+# Função auxiliar que utiliza o nome do localhost para retornar o seu IP
+def get_local_ip():
+    hostname = socket.gethostname()
+    ip_address = socket.gethostbyname(hostname)
+    return str(ip_address)
+
 def calcular_checksum(dados):
     # Se o comprimento dos dados for ímpar, adiciona um byte extra
     if len(dados) % 2 == 1:
@@ -48,50 +66,29 @@ def calcular_checksum(dados):
     # Retorna o complemento de 1 do que foi calculado
     return ~checksum & 0xFFFF
 
-def montar_cabecalho(porta_origem, porta_destino, ip_origem, ip_destino, payload):
-    # Campos do cabeçalho UDP de 8 bytes total
-    cabecalho = bytearray(8)
-    cabecalho[0:2] = porta_origem.to_bytes(2, 'big')       # Porta de origem - 2 bytes
-    cabecalho[2:4] = porta_destino.to_bytes(2, 'big')      # Porta de destino - 2 bytes
-    tam = 8 + len(payload)                       # Comprimento do cabeçalho + dados
-    cabecalho[4:6] = tam.to_bytes(2, 'big')     # Comprimento total
-    cabecalho[6:8] = (0).to_bytes(2, 'big')            # Checksum configurado como 0 - 2 bytes
-
-    # Construção do pseudo-cabeçalho IP para o cálculo do checksum
-    cabecalho_ip = struct.pack('!4s4sBBH', # Faz a formatação de cada campo da struct, definindo o seu tamanho e tipo
-                                ip_origem.packed,       # IP de origem - 4 bytes
-                                ip_destino.packed,      # IP de destino - 4 bytes
-                                0,                   # Byte reservado - 1 byte
-                                17,                  # Protocolo UDP - 1 byte
-                                tam)          # Comprimento UDP - 2 bytes
-
-    # Concatena o pseudo-cabeçalho, o cabeçalho UDP e o payload para calcular o checksum
-    dados = cabecalho_ip + cabecalho + payload
-    checksum = calcular_checksum(dados)
-    cabecalho[6:8] = checksum.to_bytes(2, 'big')
-
-    return cabecalho + payload
-
 def enviar_req(tipo):
-    # # Envia a mensagem e retorna a resposta do servidor
-    # payload, identificador = montar_req(tipo)
-    # ip = IP(dst=IP_SERV)
-    # udp = UDP(sport=random.randint(49152, 65535), dport=PORTA_SERV)
-    # pacote = ip / udp / Raw(load=payload)
-    # resposta = sr1(pacote, timeout=2)
+    # Envia a mensagem e retorna a resposta do servidor
 
-    ip_origem = IPv4Address("192.168.1.105")
-    ip_destino = IPv4Address(IP_SERV)
-
-    # Monta o payload e o cabeçalho UDP com checksum calculado
     payload, identificador = montar_req(tipo)
-    porta_origem = random.randint(49152, 65535)
-    pacote_udp = montar_cabecalho(porta_origem, PORTA_SERV, ip_origem, ip_destino, payload)
+    ip = IP(dst=IP_SERV)
+    # tam = cabeçalho de 8 bytes + tamanho do payload
+    tam = 8 + len(payload)
+    udp = UDP(sport=random.randint(49152, 65535), dport=PORTA_SERV, len = tam)
 
-    # Envia o pacote UDP usando Scapy
-    ip = IP(src=str(ip_origem), dst=IP_SERV)
-    pacote = ip / Raw(load=pacote_udp)
-    resposta = sr1(pacote, timeout=5)
+    # Junta os cabeçalhos IP e UDP com o payload
+    pacote = ip / udp / Raw(load=payload)
+
+
+    # Calculando o checksum...
+    # Cria um pseudocabecalho ip
+    cabec_ip = pseudo_cabec_ip(get_local_ip(), IP_SERV, tam)
+    # Concatena o pseudo-cabeçalho, o cabeçalho UDP e o payload para calcular o checksum
+    checksum = calcular_checksum(cabec_ip + bytes(pacote[UDP]) + bytes(pacote[Raw]))
+    print(f"Checksum: {checksum:}")
+
+    # Envia o pacote e recebe a resposta, com timeout de 2seg
+    resposta = sr1(pacote, timeout=2)
+
 
     # Exibe o status da resposta recebida
     if resposta:
@@ -104,11 +101,12 @@ def enviar_req(tipo):
 def receber_resp(resposta, tipo):
     # Processa a resposta recebida com base na opção
     if resposta and Raw in resposta:
+        # Seleciona o conteudo da resposta
         conteudo = resposta[Raw].load
-        if tipo == 3:
+        if tipo == 3: # Considera a resposta como um inteiro nos últimos 4 bytes
             resposta_formatada = int.from_bytes(conteudo[4:], byteorder='big')
             print(f"Quantidade de respostas do servidor: {resposta_formatada}")
-        else:
+        else: # Considera a resposta como uma string decodificada usando utf-8
             resposta_formatada = conteudo[4:].decode('utf-8')
             print(f"Resposta do servidor: {resposta_formatada}")
     else:
@@ -134,4 +132,3 @@ while True:
     
     if resposta:
         receber_resp(resposta, op)
-        
